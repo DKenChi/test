6c665b759ef22be1b37873c23b1cd1dbe2e65274
zhaoxye <zhaoxye@enn.cn>
2021-08-18 08:11:45 +0800

    second commit

--- src/main/java/com/zxy/git/GitTest.java
+++ src/main/java/com/zxy/git/GitTest.java
@@ -6,5 +6,6 @@ package com.zxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello world");
     }
 }
